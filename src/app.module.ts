b35bbc72232fc26749cec5097accb98c84d9bde3
developkosarev <develop.kosarev@gmail.com>
2024-04-20 11:24:50 +0200

    Add prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { CowSayModule } from './cow-say/cow-say.module';
 import { S3BucketModule } from './s3-bucket/s3-bucket.module';
 import { SayHelloModule } from './sayHello/sayHello.module';
+import { PrismaHelloModule } from './prisma-hello/prisma-hello.module';
 
 @Module({
-  imports: [S3BucketModule, CowSayModule, SayHelloModule],
+  imports: [S3BucketModule, CowSayModule, SayHelloModule, PrismaHelloModule],
   controllers: [AppController],
   providers: [AppService],
 })
