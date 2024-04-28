e07d303eeb25c8b3da3b65d688eb487e63c35911
developkosarev <develop.kosarev@gmail.com>
2024-04-28 22:49:58 +0200

    New service

--- src/tunnel-ssh/mysql-ssh.service.ts
+++ src/tunnel-ssh/mysql-ssh.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { SshConfigService } from './config/ssh.config';
 import { MysqlConfigService } from './config/mysql.config';
+import { readFileSync } from 'fs';
 
 @Injectable()
 export class MysqlSshService {
@@ -11,14 +12,13 @@ export class MysqlSshService {
 
   run()  {
     const mysqlssh = require('mysql-ssh');
-    const fs = require('fs');
 
     mysqlssh
       .connect(
         {
           host: this.sshConfigService.getHost(),
           user: this.sshConfigService.getUser(),
-          privateKey: fs.readFileSync(this.sshConfigService.getPrivateKeyPpk()),
+          privateKey: readFileSync(this.sshConfigService.getPrivateKeyPpk()),
         },
         {
           host: this.mysqlConfigService.getHost(),
@@ -30,6 +30,8 @@ export class MysqlSshService {
       .then((client) => {
         client.query('SELECT * FROM `store_website`', function (err, results, fields) {
           if (err) throw err;
+
+          //console.log(fields);
           console.log(results);
           mysqlssh.close();
         });
